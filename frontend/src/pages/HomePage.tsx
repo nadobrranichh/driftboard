2b312d1941ea11b23e6fec393597f95c4d3cddfa
nadobrranichh <nazarshch26@gmail.com>
2026-08-26 10:17:11 +0300

    feat: add new form to home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,22 @@
 import { Plus } from "lucide-react";
 import Board from "../components/Board";
+import NewBoardForm from "../components/NewBoardForm";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [isFormOpen, setIsFormOpen] = useState(false);
   return (
     <main>
+      {isFormOpen && <NewBoardForm onClose={() => setIsFormOpen(false)} />}
       <div className="flex justify-between items-center">
         <div>
           <h2 className="text-3xl font-semibold">Your boards</h2>
           <p>X boards</p>
         </div>
-        <button className="bg-primary rounded-xl cursor-pointer h-12 w-12 flex items-center justify-center">
+        <button
+          onClick={() => setIsFormOpen(true)}
+          className="bg-primary rounded-xl cursor-pointer h-12 w-12 flex items-center justify-center"
+        >
           <Plus className="text-surface" />
         </button>
       </div>
