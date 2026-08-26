9bc2abb9229c308a1477323c5ee92d29abf8417a
nadobrranichh <nazarshch26@gmail.com>
2026-08-26 10:03:10 +0300

    feat: add text sizing to input group

--- frontend/src/components/InputGroup.tsx
+++ frontend/src/components/InputGroup.tsx
@@ -1,16 +1,25 @@
+const textSizeClasses = {
+  sm: "text-sm",
+  base: "text-base",
+  lg: "text-lg",
+  xl: "text-xl",
+};
+
 export default function InputGroup({
   name,
   type = "text",
+  textSize = "sm",
 }: {
   name: string;
   type?: string;
+  textSize?: keyof typeof textSizeClasses;
 }) {
   function createLabelText(name: string) {
     return name[0].toUpperCase() + name.slice(1).replaceAll("-", " ");
   }
   return (
     <div>
-      <label htmlFor={name} className="text-sm">
+      <label htmlFor={name} className={textSizeClasses[textSize]}>
         {createLabelText(name)}
       </label>
       <input
