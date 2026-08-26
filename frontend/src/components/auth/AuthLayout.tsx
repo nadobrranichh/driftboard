3a512e5cfb1e10eb846bbcdabe6e541c7eb3aedb
nadobrranichh <nazarshch26@gmail.com>
2026-08-26 07:51:53 +0300

    feat: update auth component for new favicon

--- frontend/src/components/auth/AuthLayout.tsx
+++ frontend/src/components/auth/AuthLayout.tsx
@@ -3,13 +3,15 @@ import Logo from "/favicon.svg";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <form className="bg-surface flex flex-col gap-5 rounded-md p-5 w-80 border border-primary shadow-xl">
+    <form className="bg-surface flex flex-col gap-5 rounded-xl p-5 w-80 border border-primary shadow-xl">
       <div>
         <div className="flex items-center gap-1">
-          <img src={Logo} className="h-5" />
-          <p>DriftBoard</p>
+          <div className="bg-primary rounded-md p-1">
+            <img src={Logo} className="h-5 w-5 object-contain" />
+          </div>
+          <p className="font-bold text-primary">DriftBoard</p>
         </div>
-        <p className="text-text-muted text-sm">Work with your plans.</p>
+        <p className="text-text-muted text-sm mt-1">Work with your plans.</p>
       </div>
 
       {children}
