e790711f5932521cf342b230f3c0fb410e30f943
nadobrranichh <nazarshch26@gmail.com>
2026-08-30 09:01:49 +0300

    refator: improve button component styling and typing

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,13 +1,26 @@
 import { type ReactNode } from "react";
 
 export default function Button({
+  outlined = false,
+  className = "",
   children,
   ...props
 }: {
+  outlined?: boolean;
+  className?: string;
   children: ReactNode;
 }) {
+  const baseClasses =
+    "rounded-lg px-5 py-3 text-md font-medium transition-colors cursor-pointer";
+  const filledClasses = "bg-primary text-surface hover:bg-primary-dark";
+
+  const outlinedClasses =
+    "bg-surface border border-border text-text hover: bg-bg";
   return (
-    <button className="bg-primary text-surface rounded-md px-2 py-2" {...props}>
+    <button
+      className={`${baseClasses} ${outlined ? outlinedClasses : filledClasses} ${className}`}
+      {...props}
+    >
       {children}
     </button>
   );
