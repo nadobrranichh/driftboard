f5d5f68ab696af4d0e954c498c33225e0e315b0f
nadobrranichh <nazarshch26@gmail.com>
2026-08-30 06:41:03 +0300

    feat: add configuration options to input group

--- frontend/src/components/InputGroup.tsx
+++ frontend/src/components/InputGroup.tsx
@@ -5,29 +5,53 @@ const textSizeClasses = {
   xl: "text-xl",
 };
 
+function createLabelText(name: string) {
+  return name[0].toUpperCase() + name.slice(1).replaceAll("-", " ");
+}
+
+function getDates() {
+  const minDate = new Date();
+  const maxDate = new Date();
+  maxDate.setFullYear(maxDate.getFullYear() + 1);
+
+  return {
+    min: minDate.toISOString().split("T")[0],
+    max: maxDate.toISOString().split("T")[0],
+  };
+}
+
 export default function InputGroup({
   name,
   type = "text",
   textSize = "sm",
+  textarea = false,
 }: {
   name: string;
   type?: string;
   textSize?: keyof typeof textSizeClasses;
+  textarea?: boolean;
 }) {
-  function createLabelText(name: string) {
-    return name[0].toUpperCase() + name.slice(1).replaceAll("-", " ");
-  }
   return (
     <div>
       <label htmlFor={name} className={textSizeClasses[textSize]}>
         {createLabelText(name)}
       </label>
-      <input
-        type={type}
-        name={name}
-        id={name}
-        className="block bg-text rounded-md text-surface px-2 py-2 w-full"
-      />
+      {textarea ? (
+        <textarea
+          name={name}
+          id={name}
+          className="block bg-text rounded-md text-surface px-2 py-2 w-full"
+          rows={3}
+        ></textarea>
+      ) : (
+        <input
+          type={type}
+          name={name}
+          id={name}
+          className="block bg-text rounded-md text-surface px-2 py-2 w-full"
+          {...(type === "date" && getDates())}
+        />
+      )}
     </div>
   );
 }
