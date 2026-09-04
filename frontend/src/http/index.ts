b8d2996c06d8da2a2e4e2ba9bb59bc7a3c5c87b9
nadobrranichh <nazarshch26@gmail.com>
2026-09-04 22:34:23 +0300

    feat: add custom hooks wrapping tanstack for signup and login

--- frontend/src/http/index.ts
+++ frontend/src/http/index.ts
@@ -1,3 +1,30 @@
 import { QueryClient } from "@tanstack/react-query";
-
 export const queryClient = new QueryClient();
+
+export async function login(data: { email: string; password: string }) {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+    mode: "cors",
+  });
+  const resData = await res.json();
+  if (resData.error) throw resData;
+  return resData;
+}
+
+export async function signup(data: {
+  name: string;
+  email: string;
+  password: string;
+}) {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/auth/signup`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+    mode: "cors",
+  });
+  const resData = await res.json();
+  if (resData.error) throw resData;
+  return resData;
+}
