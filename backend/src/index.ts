91cfc3e0559f5137e8a76fa5970fe4d8b542cbdc
nadobrranichh <nazarshch26@gmail.com>
2026-09-05 08:23:25 +0300

    fix: set cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,12 @@ import tasksRouter from "./routes/task.routes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
