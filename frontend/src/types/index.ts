22d0bbc6f5577874b43c8c11f38025b92c1af7c3
nadobrranichh <nazarshch26@gmail.com>
2026-09-06 11:36:17 +0300

    feat: define task, column types

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,9 +1,28 @@
 import type { boardIcons } from "../lists/boardIconsList";
 
+export type TaskType = {
+  id: number;
+  title: string;
+  description: string;
+  dueDate: string;
+  position: number;
+  assigneeId?: number;
+  columnId: number;
+};
+
+export type ColumnType = {
+  id: number;
+  title: string;
+  position: number;
+  boardId: number;
+  tasks?: TaskType[];
+};
+
 export type BoardType = {
   id: number;
   title: string;
   icon: keyof typeof boardIcons;
   iconColor: string;
   taskCount?: number;
+  columns?: ColumnType[];
 };
