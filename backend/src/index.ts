836b1422b6041ffd435a95f14880e37c1d1eaf6c
nadobrranichh <nazarshch26@gmail.com>
2026-09-10 10:18:54 +0300

    feat: add endpoint for user lookup by their email

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth.routes.js";
 import boardsRouter from "./routes/board.routes.js";
 import columnsRouter from "./routes/column.routes.js";
 import tasksRouter from "./routes/task.routes.js";
+import usersRouter from "./routes/users.routes.js";
 
 dotenv.config();
 
@@ -27,6 +28,7 @@ app.use("/auth", authRouter);
 app.use(boardsRouter);
 app.use(columnsRouter);
 app.use(tasksRouter);
+app.use(usersRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
