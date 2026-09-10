71eb953fcd61a2e71fc7180a77d241e6f9693c5e
nadobrranichh <nazarshch26@gmail.com>
2026-09-10 14:42:39 +0300

    fix: board creation resulting in an error

--- frontend/src/hooks/useCreateBoard.ts
+++ frontend/src/hooks/useCreateBoard.ts
@@ -1,14 +1,22 @@
 import { useMutation } from "@tanstack/react-query";
-import { useNavigate } from "react-router";
 import { createBoard } from "../http/boards";
+import { queryClient } from "../http";
+import type { BoardType } from "../types";
 
 export default function useCreateBoard() {
-  const navigate = useNavigate();
-
   return useMutation({
     mutationFn: createBoard,
     onSuccess: (data) => {
-      navigate(`/board/${data.board.id}`, { state: data });
+      queryClient.setQueryData(
+        ["boards"],
+        (oldBoards: { boards: BoardType[] } | null) => {
+          const boards = oldBoards?.boards;
+          if (!boards) return oldBoards;
+          return {
+            boards: [...boards, { ...data.board, taskCount: 0 }],
+          };
+        },
+      );
     },
     onError: (err) => console.error(err),
   });
