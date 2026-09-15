ddb5a4084cef45e36ee10f286d209981d0fcbb23
nadobrranichh <nazarshch26@gmail.com>
2026-09-15 18:02:54 +0300

    feat: update column component to be a droppable container

--- frontend/src/components/Column.tsx
+++ frontend/src/components/Column.tsx
@@ -1,5 +1,10 @@
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 import type { ColumnType, TaskType } from "../types";
 import Task from "./Task";
+import { useDroppable } from "@dnd-kit/core";
 
 export default function Column({
   data,
@@ -8,13 +13,19 @@ export default function Column({
   data: ColumnType;
   onNewTask: () => void;
 }) {
+  const { setNodeRef } = useDroppable({ id: `${data.id}-column` });
   return (
-    <div className="bg-border lg:w-80 p-3 rounded-xl shrink-0">
+    <div ref={setNodeRef} className="bg-border lg:w-80 p-3 rounded-xl shrink-0">
       <p className="text-center mb-3 font-semibold">{data.title}</p>
       <div className="flex flex-col gap-3">
-        {data.tasks?.map((task: TaskType) => (
-          <Task data={task} key={task.id} />
-        ))}
+        <SortableContext
+          items={data.tasks?.map((t) => `${t.id}-task`) || []}
+          strategy={verticalListSortingStrategy}
+        >
+          {data.tasks?.map((task: TaskType) => (
+            <Task data={task} key={task.id} />
+          ))}
+        </SortableContext>
         <div
           className="border border-text-muted p-5 rounded-xl cursor-pointer"
           onClick={onNewTask}
